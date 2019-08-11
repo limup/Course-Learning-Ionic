0e03cca4c6540e8f9c2c0d4f0de505dfe928a22d
limup <limup@outlook.com>
2019-08-10 22:05:05 -0300

    New updates: Use JSON archive to showing on page feed

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -10,7 +10,28 @@ export class FeedPage implements OnInit {
   public Titulo:string = "Social Bella Lorena";
   public Autor:any = "Limup TI";
   public Corporation:string = "Limup Corp.";
-
+   
+  public json = {
+    feeds:[
+      {
+        background: "/assets/images/madison.jpg",
+        avatar: "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y",
+        autor: "Arnaldo Junior",
+        titulo: "Estou criando um app fantástico...",
+        texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen bo....",
+        tempo: "1min ago"
+      },
+      {
+        background: "/assets/images/madison.jpg",
+        avatar: "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y",
+        autor: "Enzo Soares",
+        titulo: "Atendimento Fantástico...",
+        texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has be....",
+        tempo: "2min ago"
+      }
+    ]
+  };
+  
   public somaDoisNumeros(num1:number, num2:number): void{
     alert("Minha função funcionou");
     alert(num1 + num2);
